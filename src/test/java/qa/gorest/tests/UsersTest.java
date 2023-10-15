fad39c33eeb70a7c0f78ce4f08e76958b798450d
Subodh Singh <sksingh329@gmail.com>
2023-10-15 18:21:26 +0530

    Added TC-2 -> filterUserWithNameTest

--- src/test/java/qa/gorest/tests/UsersTest.java
+++ src/test/java/qa/gorest/tests/UsersTest.java
@@ -2,20 +2,27 @@ package qa.gorest.tests;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.path.json.config.JsonPathConfig;
 import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Locale;
+
 public class UsersTest {
+    @BeforeClass
+    public void setup(){
+        RestAssured.baseURI = "https://gorest.co.in"; // set only once per application and get this value from app.env.properties file
+        RestAssured.basePath = "/public/v2/users";
+    }
     @Test
     public void listAllUsersTest(){
-        RestAssured.baseURI = "https://gorest.co.in";
-
-        Response listUserResponse = RestAssured.given()
-                .when()
-                .get("/public/v2/users");
+        Response listUserResponse = RestAssured.get();
 
         //Fetching response headers
+        int paginationLimit = Integer.parseInt(listUserResponse.getHeader("x-pagination-limit"));
+        int paginationTotal = Integer.parseInt(listUserResponse.getHeader("x-pagination-total"));
         System.out.println(listUserResponse.getHeader("Content-Type"));
         System.out.println(listUserResponse.getHeader("x-pagination-page"));
         System.out.println(listUserResponse.getHeader("x-pagination-limit"));
@@ -29,5 +36,41 @@ public class UsersTest {
         System.out.println(responseBodyJsonPath.getString("[0].email"));
         System.out.println(responseBodyJsonPath.getString("[0].gender"));
         System.out.println(responseBodyJsonPath.getString("[0].status"));
+
+        //Get the count of users in response
+        int userCount = responseBodyJsonPath.getList("$").size();
+        System.out.println(userCount);
+
+        if(paginationTotal > 10){
+            Assert.assertEquals(userCount,paginationLimit);
+        }
+        else{
+            Assert.assertEquals(userCount,paginationTotal);
+        }
+    }
+    @Test
+    public void filterUserWithNameTest(){
+        Response filterUserResponse = RestAssured.given()
+                .queryParam("name","kumar")
+                .get();
+
+        JsonPath filterUserResponseJsonPath = filterUserResponse.jsonPath();
+
+        List<String> userNames = filterUserResponseJsonPath.getList("name");
+        System.out.println(userNames);
+        for (String userName : userNames){
+            Assert.assertTrue(userName.toLowerCase(Locale.ROOT).contains("kumar"));
+        }
+
+        int paginationLimit = Integer.parseInt(filterUserResponse.getHeader("x-pagination-limit"));
+        int paginationTotal = Integer.parseInt(filterUserResponse.getHeader("x-pagination-total"));
+        int userCount = filterUserResponseJsonPath.getList("$").size();
+
+        if(paginationTotal > 10){
+            Assert.assertEquals(userCount,paginationLimit);
+        }
+        else{
+            Assert.assertEquals(userCount,paginationTotal);
+        }
     }
 }
